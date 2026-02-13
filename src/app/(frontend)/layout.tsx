330f906874baf7359ff2dbfcf9a643dc2f199d7e
Gunnar Saliev <g.saliev@gmail.com>
2026-02-13 14:13:19 +0200

    added provider for heroui

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './styles.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { HeroUIProvider } from '@heroui/react'
+import { AuthProvider } from '@/providers/Auth'
 import { Navbar17 } from '@/components/navbar17'
 import { Footer16 } from '@/components/footer16'
 
@@ -15,18 +17,22 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <Navbar17 />
-            {children}
-            <Footer16 />
-          </main>
-        </ThemeProvider>
+        <HeroUIProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <AuthProvider>
+              <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <Navbar17 />
+                {children}
+                <Footer16 />
+              </main>
+            </AuthProvider>
+          </ThemeProvider>
+        </HeroUIProvider>
       </body>
     </html>
   )
